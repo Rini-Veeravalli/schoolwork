48ff5abff5605702ff5de6541d43ec4a8499abd3
Andrew <andrew.chan9494@yahoo.com>
2015-03-05 02:40:01 +0000

    Version3

--- codewordmain.c
+++ codewordmain.c
@@ -3,39 +3,6 @@
 #include<stdio.h>
 #include<string.h>
 
-#define MODE argv[1]
-
-void usageinfo();
-int main(int argc, char *argv[])
-{
-  /*help message */
-  if(argc == 1 || strcmp(MODE, "-h") == 0 || strcmp(MODE, "--help") == 0)
-    {
-      usageinfo();
-      return 0;
-    }
-  
-  if(strcmp(MODE, "--spellcheck") != 0)
-    {
-      fprintf(stderr, "Invalid mode \"%s\"\n", MODE);
-      return 1;
-    }
-  else if(strcmp(MODE, "--spellcheck") == 0 && argc < 4)
-    {
-      fprintf(stderr, "Usage: codeword.exe --spellcheck DICTFILE WORDS\n");
-      return 1;
-    }
-}
-
-
-
-
-
-
-
-
-
-
 
 /*help message */
 void usageinfo()
@@ -48,4 +15,85 @@ void usageinfo()
   printf("codeword.exe --codeword-show CSVFILE         : Show codeword from csv file.\n");
 }
 
+int error_messages(char *arg, int commandline, char *accessfile )
+{
+  if(strcmp(arg, "--spellcheck") != 0)
+    {
+      fprintf(stderr, "Invalid mode \"%s\"\n", arg);
+      return 1;
+    }
+  else if(strcmp(arg, "--spellcheck") == 0 && commandline < 4)
+    {
+      fprintf(stderr, "Usage: codeword.exe --spellcheck DICTFILE WORDS\n");
+      return 1; 
+    }
+  
 
+  if(!strcmp(accessfile, "text_file/words.txt") == 0 || !strcmp(accessfile, "text_file/words3.txt") == 0)
+    {
+      fprintf(stderr, "Error: \"%s\" does not exist.\n", accessfile);
+      return 1;
+    }
+  return 1; 
+}
+
+int openfile(char *arg)
+{
+  if(strcmp(arg, "text_file/words.txt") == 0)
+    {
+      char *dictfile = "text_file/words.txt";
+      FILE *file = fopen(dictfile, "r");
+      if(file == NULL)
+        {
+          fprintf(stderr, "Unable to open \"%s\"", dictfile);
+          return 1;
+        }
+      
+    }
+  else if(strcmp(arg, "text_file/words3.txt") == 0)
+    {
+      char *dictfile = "text_file/words3.txt";
+      FILE *file = fopen(dictfile, "r");
+      if(file == NULL)
+        {
+          fprintf(stderr, "Unable to open \"%s\"", dictfile);
+          return 1;
+        }
+    }
+  return 1;
+}
+
+
+
+
+/*main*/
+int main(int argc, char *argv[])
+{
+  char *Mode = argv[1];
+  char *whichFile = argv[2];
+  /*help message */
+  if(argc == 1 || strcmp(Mode, "-h") == 0 || strcmp(Mode, "--help") == 0)
+    {
+      usageinfo();
+      return 0;
+    }
+  /* error messages */ 
+  if(error_messages(Mode, argc, whichFile) == 0)
+    {
+      return 0;
+    }
+  else
+    {
+      return 1;
+    }
+
+  /*readfile*/
+  if(openfile(whichFile) == 0)
+    {
+      return 0;
+    }
+  else
+    {
+      return 1;
+    }
+}
